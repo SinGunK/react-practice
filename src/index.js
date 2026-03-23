cb4fba1736262c0b86db7335e971643b2298fc0e
강신건 <singun@gangsingeon-ui-MacBookPro.local>
2026-03-23 17:42:18 +0900

    03.23 17:42

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Liverpool from './Liverpool'; //''는 기초 컴포넌트 설정
-
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Liverpool />
+    <App />
   </React.StrictMode>
-); //Practice 부분 역시 자기자신이 설정.
+);
