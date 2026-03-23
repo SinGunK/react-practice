2419e794cac1cf597d2bd6b10f0191cfcea4274a
강신건 <singun@gangsingeon-ui-MacBookPro.local>
2026-03-23 17:10:46 +0900

    react practice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Liverpool from './Liverpool'; //''는 기초 컴포넌트 설정
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Liverpool />
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+); //Practice 부분 역시 자기자신이 설정.
